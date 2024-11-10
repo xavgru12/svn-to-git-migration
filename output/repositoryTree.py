57f84f9b526613ffc2ab38ae79c029aa9c299e00
Xaver Gruber <xaver.gruber@curtis.ch>
2024-11-10 15:24:01 +0100

    delete moved functions in repositoryTree

--- output/repositoryTree.py
+++ output/repositoryTree.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from typing import Any
 from typing import Optional
 import os
-import shutil
 
 import model.svnRepositoryModel
 import parser.svnRepositoryParser
@@ -10,13 +9,10 @@ import data.configuration as configuration
 import output.git_checkout
 import output.migration
 import output.logger
-
 import output.external_subfolder_migration
 import parser.branchConfigurationParser
 import output.transformation
-import execution.subprocess_execution
-import execution.shutil_execution
-import execution.git_execution
+import output.git_checkout
 
 import pdb
 
@@ -139,10 +135,10 @@ class RecursiveList:
 
     def find_nodes(self, var=None):
         if var is None:
-            self.checkout_top_repository(self.current)
+            output.git_checkout.checkout_top_repository(self.current)
 
         if var is not None:
-            self.clone_repository(self.current)
+            output.git_checkout.clone_repository(self.current)
 
         if self.dependencies:
             print("has dependencies")
@@ -151,7 +147,7 @@ class RecursiveList:
             for dependency in self.dependencies:
                 dependency.find_nodes("test")
 
-                self.add_submodule(dependency.current)
+                output.git_checkout.add_submodule(dependency.current)
 
             if self.current.folder_name == "ag-mobile-app":
                 print("parsed recursively the mobile app")
@@ -160,154 +156,13 @@ class RecursiveList:
                 working_directory = os.path.join(self.current.local_folder_path, self.current.folder_name)
             else:
                 working_directory = self.current.local_folder_path
-            self.create_and_push_commit(self.current, working_directory)
+            output.git_checkout.create_and_push_commit(self.current, working_directory)
         else:
             pass
             # print("no more dependencies")
             # print("current: ")
             # print(self.current)
 
-
-    def clone_repository(self, repository):
-        repository_name = parser.branchConfigurationParser.parse_repo_name(repository.remote_path)
-        repository_name_no_externals = f"{repository_name}-no-externals"
-        folder_name = repository.folder_name
-        local_folder_path = repository.local_folder_path
-
-        clone_command = f"git submodule add --force git@bitbucket.org:curtisinst/{repository_name_no_externals}.git ./{folder_name}"
-
-        if folder_name.endswith(".cs"):
-            print(f"path is file (no submodule): {folder_name}")
-            return
-
-        os.makedirs(local_folder_path, exist_ok=True)
-
-        execution.subprocess_execution.check_output_execute(
-                clone_command, local_folder_path
-            )
-        
-        repository_folder = os.path.join(local_folder_path, folder_name)
-
-        print_mode = True
-
-        execution.git_execution.add_remote_upload(repository_name, repository_folder)
-        if execution.git_execution.check_remote_upload_exists(repository_folder):
-            push_repository_as_live = "git push upload --mirror"
-            execution.subprocess_execution.check_output_execute(
-                    push_repository_as_live, repository_folder
-                )
-        else:
-            if print_mode:
-                self.add_missing_remote_to_file(repository_name)
-                print(f"added missing remote: {repository_name}")
-            else:
-                raise ValueError(
-                    f'error: remote origin for repository: "{repository_name}" does not exist'
-                )
-
-
-    def create_and_push_commit(self, repository, working_directory):
-        print("push repository:")
-        print(repository)
-        repository_name = parser.branchConfigurationParser.parse_repo_name(repository.remote_path)
-
-        add_command = "git add -A"
-        execution.subprocess_execution.check_output_execute(
-                add_command, working_directory
-            )
-
-        commit_command = ["git", "commit", "-m", "activate git submodules"]
-        execution.subprocess_execution.check_output_execute(
-                commit_command, working_directory
-            )
-        
-        print_mode = True
-
-        if execution.git_execution.check_remote_upload_exists(working_directory):
-            push_command = "git push upload main --force"
-            execution.subprocess_execution.check_output_execute(
-                    push_command, working_directory
-                )
-        else:
-            if print_mode:
-                self.add_missing_remote_to_file(repository_name)
-                print(f"added missing remote: {repository_name}")
-            else:
-                raise ValueError(
-                    f'error: remote origin for repository: "{repository_name}" does not exist'
-                )
-
-    def add_missing_remote_to_file(self, name):
-        file_path = "remotes.txt"
-
-        with open(file_path, "a") as file:
-            file.write(f"{name}\n")
-
-    def checkout_top_repository(self, repository):
-        repository_name = parser.branchConfigurationParser.parse_repo_name(repository.remote_path)
-        repository_name_no_externals = f"{repository_name}-no-externals"
-
-        local_folder_path = repository.local_folder_path
-        folder_name = os.path.basename(local_folder_path)
-        base_directory = os.path.dirname(local_folder_path)
-
-        execution.shutil_execution.delete(local_folder_path)
-
-        command = f"git clone git@bitbucket.org:curtisinst/{repository_name_no_externals}.git ./{folder_name}"
-        print(command)
-
-        execution.subprocess_execution.check_output_execute(
-                command, base_directory
-            )
-        execution.git_execution.add_remote_upload(repository_name, local_folder_path)
-
-
-    def add_submodule(self, repository):
-        repository_name = parser.branchConfigurationParser.parse_repo_name(repository.remote_path)
-
-        local_folder_path = repository.local_folder_path
-        folder_name = repository.folder_name
-
-        if folder_name.endswith(".cs"):
-            print(f"path is file (no submodule): {folder_name}")
-            return
-
-        root_git_path = self.get_root_git_path(local_folder_path)
-        subpath = local_folder_path[len(root_git_path) + 1:]
-        print(subpath)
-
-        self.git_delete_folder(folder_name, local_folder_path, subpath, root_git_path)
-        execution.shutil_execution.delete(os.path.join(local_folder_path, folder_name))
-        os.makedirs(local_folder_path, exist_ok=True)
-
-        command = f"git submodule add --force git@bitbucket.org:curtisinst/{repository_name}.git ./{folder_name}"
-        print(command)
-
-        execution.subprocess_execution.check_output_execute(
-                command, local_folder_path
-            )
-
-    def get_root_git_path(self, local_folder_path):
-        local_path = configuration.get_local_path()
-        remove_folder = local_folder_path
-        root_git_path = local_folder_path
-
-        while remove_folder != local_path:
-            root_git_path = remove_folder
-            remove_folder = os.path.dirname(remove_folder)
-
-        print(f"root git path: {root_git_path}")
-        return root_git_path
-
-
-    def git_delete_folder(self, folder, path, relative_path, root_git_path):
-        if os.path.exists(os.path.join(path, folder)):
-            command = f"git rm {folder} -rf"
-            execution.subprocess_execution.check_output_execute(
-                    command, path
-                )
-            execution.shutil_execution.delete(os.path.join(root_git_path, f".git/modules/{relative_path}/{folder}"))
-        
 
 class RepositoryTree:
     recursive_list = Optional[RecursiveList]
